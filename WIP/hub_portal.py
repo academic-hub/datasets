03069608cb50a448d0d6753424d3652269d2a91c
Christian Foisy <cfoisy@osisoft.com>
2021-05-26 11:12:30 -0400

    Plot button, plot only with numerical columns

--- WIP/hub_portal.py
+++ WIP/hub_portal.py
@@ -9,18 +9,24 @@ import plotly.express as px
 import time
 import base64
 
-st.title('Hub Data View Portal - v0.9')
+st.title('Hub Data View Portal - v0.95')
 
 
 @st.cache(allow_output_mutation=True)
 def hub_client():
     hub_ocs = HubClient()
     hub_ocs.refresh_datasets(
-        # endpoint=graphql_endpoint,
         experimental=True,
         additional_status="eds_onboarding",
     )
     return hub_ocs
+
+
+@st.cache(show_spinner=False)
+def get_data_view(dataset, asset, start_time, end_time, interpolation):
+    return hub.dataview_interpolated_pd(hub.namespace_of(dataset),
+                                        hub.asset_dataviews(filter='', asset=asset)[0],
+                                        start_time.isoformat(), end_time.isoformat(), interpolation)
 
 
 def get_table_download_link_csv(df):
@@ -55,26 +61,29 @@ delta = datetime.timedelta(seconds=pytimeparse.parse(window))
 end_time = start_time + delta
 
 st.write("Current dataset: ", dataset, " || Selected asset:", asset)
-st.write("Start time || End time :", start_time.isoformat(), "||", end_time.isoformat())
+st.write("Start time:", start_time.isoformat(), "||", "End time :", end_time.isoformat())
 st.write("Interpolation interval (HH:MM:SS):", interpolation)
-st.write("OCS namespace:", hub.namespace_of(dataset), "Default data view ID:",
+st.write("OCS namespace:", hub.namespace_of(dataset), "||", "Default data view ID:",
          hub.asset_dataviews(filter="", asset=asset)[0])
 
-if not csv_button:
+if 'df' not in globals():
     df = pd.DataFrame()
-else:
+
+if csv_button:
     with st.spinner(text='Getting CSV from OCS Data View...'):
-        df = hub.dataview_interpolated_pd(hub.namespace_of(dataset),
-                                          hub.asset_dataviews(filter="", asset=asset)[0],
-                                          start_time.isoformat(), end_time.isoformat(), interpolation)
+        df = get_data_view(dataset, asset, start_time, end_time, interpolation)
     st.success('Data View Request Complete!!')
     st.markdown(get_table_download_link_csv(df), unsafe_allow_html=True)
-    # "May 7, 2021, 2:03:35 PM", "May 8, 2021, 4:37:14 PM", "00:00:10")
 
 st.write(df)
-if len(df) > 0:
+
+with st.form(key="plot"):
+    plot_button = st.form_submit_button(label="(Optional step) Click here for line plot")
+
+if plot_button:
+    df = get_data_view(dataset, asset, start_time, end_time, interpolation)
     st.write("Data line plot")
     with st.spinner(text='Plotting data...'):
-        columns = list(df.columns)[2:]
+        columns = list(df.select_dtypes(include='number').columns)
         st.write(px.line(df, x="Timestamp", y=columns))
-    st.balloons()
+    st.success("Done!")
